6da2d4f9d98c971c734db1dc944b9c09870f74ba
Qamar <Qamar@MSI>
2024-04-27 19:52:15 +0300

    Vertex Class update

---
+++ src/cpcs361group15pourcode/zip/CPCS361Group15POurCodeZip.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cpcs361group15pourcode.zip;
+
+/**
+ *
+ * @author Qamar
+ */
+public class CPCS361Group15POurCodeZip {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
